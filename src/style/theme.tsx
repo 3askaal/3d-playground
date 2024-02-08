503a0b00dd61b81c63e4e1124e1e5eeb044685a2
Bas Kaal <3askaal@gmail.com>
2024-02-08 14:00:43 +0100

    install dependencies, improve project structure

---
+++ src/style/theme.tsx
@@ -0,0 +1,12 @@
+import { darken, brighten } from '3oilerplate'
+
+const black = brighten('#000', .25);
+const white = darken('#fff', .25);
+
+export const THEME = {
+  rootFontSizes: ['10px', '16px'],
+  colors: {
+    black,
+    white,
+  }
+}
