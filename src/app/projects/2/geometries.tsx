47aabfff500c381aa0ffbc0f17a70af9d31128fa
Bas Kaal <3askaal@gmail.com>
2024-10-27 23:03:29 +0100

    implement noise to generate terrain geometry

--- src/app/projects/2/geometries.tsx
+++ src/app/projects/2/geometries.tsx
@@ -1,9 +1,36 @@
 import { Color, Float32BufferAttribute, PlaneGeometry, SRGBColorSpace, Vector3 } from 'three'
-import TinyColor from 'tinycolor2'
+import { createNoise2D, NoiseFunction2D } from 'simplex-noise'
 import { random } from 'lodash'
+import TinyColor from 'tinycolor2'
 
+let noise2D: NoiseFunction2D | null = null
 const vertex = new Vector3()
 const vertexColor = new Color()
+
+const map = (val: number, smin: number, smax: number, emin: number, emax: number) => {
+  const t = (val - smin) / (smax - smin)
+  return (emax - emin) * t + emin
+}
+
+const noise = (x: number, y: number) => {
+  return map(noise2D!(x, y), -1, 1, 0, 1)
+}
+
+const octave = (x: number, y: number, octaves: number) => {
+  let val = 0
+  let freq = 1
+  let max = 0
+  let amp = 1
+
+  for (let i = 0; i < octaves; i++) {
+    val += noise(x * freq, y * freq) * amp
+    max += amp
+    amp /= 2
+    freq *= 2
+  }
+
+  return val / max
+}
 
 const getRandomTone = (color: any): [number, number, number] => {
   const modifier = random(-50, 50)
@@ -14,35 +41,59 @@ const getRandomTone = (color: any): [number, number, number] => {
   return [r, g, b]
 }
 
-export const getTerrainGeometry = ({ size, color, depth }: any) => {
+const generateTexture = (size: number) => {
+  const canvas = document.createElement('canvas')
+  canvas.width = size
+  canvas.height = size
+
+  const context = canvas.getContext('2d')!
+  context.fillStyle = 'black'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+
+  for (let i = 0; i < canvas.width; i++) {
+    for (let j = 0; j < canvas.height; j++) {
+      const v = octave(i / canvas.width, j / canvas.height, 16)
+      const per = (100 * v).toFixed(2) + '%'
+      context.fillStyle = `rgb(${per},${per},${per})`
+      context.fillRect(i, j, 1, 1)
+    }
+  }
+
+  return context.getImageData(0, 0, canvas.width, canvas.height)
+}
+
+export const getTerrainGeometry = ({ size, color, height }: any) => {
+  noise2D = createNoise2D()
+
+  const texture = generateTexture((size / 20) * 1.1)
+
   let terrainGeometry = new PlaneGeometry(size, size, size / 20, size / 20)
   terrainGeometry.rotateX(-Math.PI / 2)
 
   let position = terrainGeometry.attributes.position
 
   for (let i = 0; i < position.count; i++) {
+    const redValue = texture.data[Math.floor(i * 4)]
+
     vertex.fromBufferAttribute(position, i)
+    vertex.y += map(redValue, 0, 255, -10, 10) * (height / 10)
 
-    vertex.x += (Math.random() * (size / 100)) - (size / 200)
-    vertex.y += (Math.random() * depth)
-    vertex.z += (Math.random() * (size / 100)) - (size / 200)
-
-    position.setXYZ(i, vertex.x, vertex.y, vertex.z)
+    position.setY(i, vertex.y)
   }
 
   terrainGeometry = terrainGeometry.toNonIndexed() as any
 
   position = terrainGeometry.attributes.position
 
-  const colorsFloor = []
+  const vertexColors = []
 
   for (let i = 0; i < position.count; i++) {
     const rgb = new TinyColor(color).toRgb()
     vertexColor.setRGB(...getRandomTone(rgb), SRGBColorSpace)
-    colorsFloor.push(vertexColor.r, vertexColor.g, vertexColor.b)
+    vertexColors.push(vertexColor.r, vertexColor.g, vertexColor.b)
   }
 
-  terrainGeometry.setAttribute('color', new Float32BufferAttribute(colorsFloor, 3))
+  terrainGeometry.setAttribute('color', new Float32BufferAttribute(vertexColors, 3))
 
   return terrainGeometry
 }
--- src/app/projects/2/page.tsx
+++ src/app/projects/2/page.tsx
@@ -4,21 +4,26 @@ import { Canvas } from '@react-three/fiber'
 import { Box } from '3oilerplate'
 import { Scene } from './Scene'
 import { useConfig, makeSeparator, makeButton, makeFolder } from '@/app/hooks/useConfig'
+import { useReducer } from 'react'
 
 const Page = () => {
+  const [, regenerate] = useReducer(value => value + 1, 0)
+
   const { config, reset } = useConfig({
     ...makeFolder('config', {
       color: { value: '#ff0000' },
       bgColor: { value: '#1c1c1c' },
       size: { value: 300, min: 100, max: 1000, step: 10 },
-      depth: { value: 20, min: 0, max: 100, step: 1 },
+      height: { value: 100, min: 0, max: 200, step: 1 },
       posY: { value: -25, min: -50, max: 50, step: 1 },
       ...makeSeparator(),
       sceneRotation: { label: 'scene ⟳', value: 5, min: -20, max: 20, step: 1 },
       zoom: { value: 120, min: 1, max: 500, step: 10 },
       ...makeSeparator(),
       lights: { value: true },
-      wireframe: { value: false },
+      wireframe: { value: true },
+      ...makeSeparator(),
+      ...makeButton('regenerate', () => regenerate()),
       ...makeSeparator(),
       ...makeButton('reset', () => reset())
     })
